f67d211f9ac815c3c54ca1c3b6f551b5420fe2b0
FIRO <72495729+iamfiro@users.noreply.github.com>
2025-08-28 16:45:32 +0900

    Update ESLint configuration to use new plugins and TypeScript support; add dependencies for ESLint, Prettier, and React plugins.

--- src/components/share/Header/index.tsx
+++ src/components/share/Header/index.tsx
@@ -1,9 +1,11 @@
-import styles from './styles.module.scss'
+import Image from "next/image";
 
-export default function Index(){
-    return(
-        <div className={styles.container}>
+import s from "./styles.module.scss";
 
-        </div>
-    )
+export default function Index() {
+  return (
+    <div className={s.container}>
+      <Image src={"logo.svg"} alt="Logo" width={32} height={32} />
+    </div>
+  );
 }

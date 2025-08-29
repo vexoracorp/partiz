53623766d83753bde09217343569e44c22d0a4ec
FIRO <72495729+iamfiro@users.noreply.github.com>
2025-08-29 22:26:58 +0900

    implement button components

---
+++ src/components/ui/button/index.tsx
@@ -0,0 +1,53 @@
+import type { ButtonHTMLAttributes, ReactNode } from "react";
+
+import s from "./style.module.scss";
+
+export interface ButtonProps extends ButtonHTMLAttributes<HTMLButtonElement> {
+  children: ReactNode;
+  size?: "large" | "medium";
+  variant?: "primary" | "secondary";
+  pending?: boolean;
+  fullWidth?: boolean;
+  leadingIcon?: ReactNode;
+  trailingIcon?: ReactNode;
+}
+
+export default function Button({
+  children,
+  size = "medium",
+  variant = "primary",
+  pending = false,
+  disabled,
+  className,
+  fullWidth = false,
+  leadingIcon,
+  trailingIcon,
+  ...props
+}: ButtonProps) {
+  const buttonClassName = [
+    s.button,
+    s[size],
+    s[variant],
+    pending && s.pending,
+    disabled && s.disabled,
+    className,
+    fullWidth && s.fullWidth,
+  ]
+    .filter(Boolean)
+    .join(" ");
+
+  return (
+    <button
+      className={buttonClassName}
+      disabled={disabled || pending}
+      {...props}
+    >
+      {pending && <div className={s.spinner} />}
+      <div className={pending ? s.hiddenContent : s.buttonContent}>
+        {leadingIcon && <span className={s.leadingIcon}>{leadingIcon}</span>}
+        <span className={s.text}>{children}</span>
+        {trailingIcon && <span className={s.trailingIcon}>{trailingIcon}</span>}
+      </div>
+    </button>
+  );
+}
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,6 +1,8 @@
 import { useTranslation } from "react-i18next";
 
 import { MainLayout } from "@/components/layouts";
+import Button from "@/components/ui/button";
+import Spacing from "@/components/ui/spacing";
 
 const Home = () => {
   const { t } = useTranslation();
@@ -8,6 +10,8 @@ const Home = () => {
   return (
     <MainLayout>
       <p>{t("greeting", { name: 10 })}</p>
+      <Spacing size={16} />
+      <Button variant="secondary">Click me</Button>
     </MainLayout>
   );
 };

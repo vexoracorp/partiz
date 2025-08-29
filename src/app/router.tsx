fbf3f62c71db5fcf941e4eb5806eacfdb9cecf40
FIRO <72495729+iamfiro@users.noreply.github.com>
2025-08-29 20:12:00 +0900

    기본적인 폴더 구조 세팅

---
+++ src/app/router.tsx
@@ -0,0 +1,12 @@
+import { createBrowserRouter } from "react-router-dom";
+
+import App from "./router/App";
+
+const Router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+  },
+]);
+
+export default Router;
--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,15 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import Router from './router'
-import { RouterProvider } from 'react-router-dom'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import { RouterProvider } from "react-router-dom";
 
-const rootElement = document.getElementById('root')
-if (!rootElement) throw new Error('Root element not found')
+import Router from "./app/router";
+
+import "./index.css";
+
+const rootElement = document.getElementById("root");
+if (!rootElement) throw new Error("Root element not found");
 createRoot(rootElement).render(
   <StrictMode>
     <RouterProvider router={Router} />
   </StrictMode>,
-)
+);

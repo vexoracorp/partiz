1239ed0e82bb1f8e7c601485671562db0d0e7446
kimdohyun <dohyun2008@kakao.com>
2025-08-29 17:19:47 +0900

    Router setting

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,13 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import App from './App.tsx'
+import Router from './router'
+import { RouterProvider } from 'react-router-dom'
 
-createRoot(document.getElementById('root')!).render(
+const rootElement = document.getElementById('root')
+if (!rootElement) throw new Error('Root element not found')
+createRoot(rootElement).render(
   <StrictMode>
-    <App />
+    <RouterProvider router={Router} />
   </StrictMode>,
 )

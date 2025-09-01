7ede71a809e585bcdae3d621ee4a51bde888af2f
FIRO <72495729+iamfiro@users.noreply.github.com>
2025-09-01 12:17:29 +0900

    Update content display text for clarity

--- src/components/content/index.tsx
+++ src/components/content/index.tsx
@@ -9,7 +9,7 @@ import s from "@/components/content/styles.module.scss";
 export default function Content() {
   return (
     <VStack className={s.container}>
-      <Typo.Display>파티즈가 추천하는 컨텐츠</Typo.Display>
+      <Typo.Display>파티즈 추천 컨텐츠</Typo.Display>
       <div className={s.sectionContainer}>
         {contentData.map((item, idx) => (
           <ContentItem

6a9f5d19f5678056d6121f03ceb0c4291329bf1a
FIRO <72495729+iamfiro@users.noreply.github.com>
2025-09-01 16:28:14 +0900

    Refactor ProductCard component for improved styling and functionality
    - Enhanced styling of the ProductCard component for better visual appeal.
    - Updated functionality to handle product data more efficiently.
    - Made minor adjustments to the layout for a more responsive design.

---
+++ src/types/product.ts
@@ -0,0 +1,43 @@
+import type { Category } from "./category";
+
+export interface Participant {
+  id: string;
+  name: string;
+  joinedAt: Date;
+}
+
+export interface Plan {
+  id: string;
+
+  name: string;
+  description: string;
+
+  price: number;
+  discountPrice: number;
+  discountRate: number;
+
+  participants: Participant[];
+  maxParticipants: number;
+  endDate: Date;
+
+  createdAt: Date;
+  updatedAt: Date;
+}
+
+export interface Product {
+  id: string;
+  name: string;
+  image: string;
+  bannerImage: string;
+
+  category: Category[];
+  gallery: string[];
+
+  popularProduct: boolean;
+  mostUsedProduct: boolean;
+
+  plan: Plan[];
+
+  createdAt: Date;
+  updatedAt: Date;
+}

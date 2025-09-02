d55d3a951feebad42da22847bc874771f336db08
FIRO <72495729+iamfiro@users.noreply.github.com>
2025-09-02 18:11:20 +0900

    feat: test husky

--- src/pages/auth/login.tsx
+++ src/pages/auth/login.tsx
@@ -7,10 +7,8 @@ export default function Login() {
     <>
       <Header />
       <AuthLayout>
-        
-            <LoginModal />
-
+        <LoginModal />
       </AuthLayout>
     </>
   );
-};
+}

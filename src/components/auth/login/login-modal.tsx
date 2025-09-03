358bc7f8928d642f86249b4a4c24bb0a4ff23d89
orangejack <79419803+kingcat47@users.noreply.github.com>
2025-09-03 17:24:03 +0900

    로그인 페이지 수정정

--- src/components/auth/login/login-modal.tsx
+++ src/components/auth/login/login-modal.tsx
@@ -1,61 +1,16 @@
-import { useState } from "react";
-
-import { Button, Input, Spacing } from "@/components/ui";
+import { Button, Spacing } from "@/components/ui";
 
 import s from "./styles.module.scss";
 
 export default function LoginModal() {
-  const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
-
-  const handleLogin = () => {
-    if (!email.trim()) {
-      alert("아이디 또는 이메일을 입력해주세요.");
-      return;
-    }
-
-    if (!password.trim()) {
-      alert("비밀번호를 입력해주세요.");
-      return;
-    }
-
-    // 로그인 로직
-    console.log("로그인 진행", { email, password });
-  };
 
   return (
     <>
       <div className={s.container}>
         <img src="/partiz.svg" alt="logo" width="111" height="32" />
         <Spacing size={12} />
-        <div className={s.input_container}>
-          <Input
-            placeholder="아이디 또는 이메일"
-            size="large"
-            fullWidth
-            value={email}
-            onChange={(e) => setEmail(e.target.value)}
-          />
-          <Spacing size={12} />
-          <Input
-            type="password"
-            placeholder="비밀번호를 입력해주세요."
-            size="large"
-            fullWidth
-            value={password}
-            onChange={(e) => setPassword(e.target.value)}
-          />
-          <Spacing size={12} />
-          <Button
-            size="large"
-            variant="primary"
-            fullWidth
-            onClick={handleLogin}
-          >
-            로그인
-          </Button>
+        <div className={s.login_container}>
         </div>
-        <div className={s.divider} />
         <Button
           size="large"
           variant="secondary"
@@ -87,13 +42,7 @@ export default function LoginModal() {
         >
           카카오톡으로 시작하기
         </Button>
-        <div className={s.text_container}>
-          <span className={s.text}>아직 계정이 없으신가요?</span>
-          <a href="/auth/signup" className={s.signup_text}>
-            회원가입
-          </a>
         </div>
-      </div>
-    </>
+      </>
   );
 }
